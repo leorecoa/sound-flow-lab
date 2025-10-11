d99b222277c0abf633e8352d5194b3a4ff81c430
leorecoa <leorecoa2@gmail.com>
2025-10-11 03:22:41 -0300

    atualizações do build

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage: React.FC = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Settings</h1>
+      <p>Configurações do Sound Flow Lab.</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
