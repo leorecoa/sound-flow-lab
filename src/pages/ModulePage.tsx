4eadac1806a285c17a47689c45e6b8987ed2f946
leorecoa <leorecoa2@gmail.com>
2025-10-10 21:20:01 -0300

    feat: atualizar importações e scripts no projeto para melhor organização e uso do Vite

--- src/pages/ModulePage.tsx
+++ src/pages/ModulePage.tsx
@@ -7,7 +7,7 @@ import { ExampleCard } from "@/components/ExampleCard";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { toast } from "sonner";
-import { useMediaRecorder } from "../hooks/useMediaRecorder";
+import { useMediaRecorder } from "@/hooks/useMediaRecorder";
 import { useXP } from "@/hooks/useXP";
 import { MultipleChoiceExercise } from "@/components/MultipleChoiceExercise";
 import { SoundWave } from "@/components/SoundWave";
